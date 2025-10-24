3c304312a741e1a672ae62dedf5e24b605dbe053
SriPriyan <priyan07102006@gmail.com>
2025-10-24 20:22:21 +0530

    Update components, packages and page/Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import SubHero from "@/components/subHero";
-import { HyperText } from "@/components/ui/hyper-text";
-import { tr } from "framer-motion/client";
+import RadialOrbitalTimeline from "@/components/ui/radial-orbital-timeline";
+import CategoryListDemo from "@/components/demo/category-list-demo";
+
 export default function Home() {
   return (
     <>
@@ -11,48 +11,17 @@ export default function Home() {
           <h1 className="text-5xl text-accent">Note:video</h1>
         </div>
       </section>
-      <section id="sub-hero" className="min-h-screen ">
-        <div className="h-full w-full flex items-center justify-center">
-          <div className="flex items-center justify-center w-full min-h-screen bg-gray-100 dark:bg-black p-4">
-            <div className="w-full h-screen relative">
-              <SubHero
-                beamWidth={2}
-                beamHeight={15}
-                beamNumber={12}
-                lightColor="#ffffff"
-                speed={3}
-                noiseIntensity={1.75}
-                scale={0.2}
-                rotation={0}
-              />
-              <div className="absolute inset-0 flex items-center justify-center ">
-                <div className="flex justify-between w-xl">
-                  <HyperText
-                    animateOnHover={true}
-                    startOnView={true}
-                    className="text-9xl text-foreground mr-4"
-                  >
-                    Defy
-                  </HyperText>
-                  <HyperText
-                    animateOnHover={true}
-                    startOnView={true}
-                    className="text-9xl text-primary "
-                  >
-                    26
-                  </HyperText>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* <h1 className="font-bold text-6xl text-primary">Sub Section</h1> */}
+      <section
+        id="tracks"
+        className="min-h-screen flex items-center justify-center"
+      >
+        <CategoryListDemo />
       </section>
       <section
         id="timeline"
-        className="min-h-screen flex items-center justify-center"
+        className="min-h-screen"
       >
-        <h1 className="font-bold text-6xl text-primary">Timeline Section</h1>
+        <RadialOrbitalTimeline />
       </section>
 
       <section
